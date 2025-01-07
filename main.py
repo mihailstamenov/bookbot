d03464eda3c3bddbece9778ea716e1eaf69abfa8
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-07 22:39:46 +0200

    Refactor main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 def main ():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = get_word_count(text)
+    print(f"{word_count} words found in the document")
     
-    split = file_contents.split()
-    print(len(split))
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+
+def get_word_count(text):
+    split = text.split()
+    return len(split)
+
 
 main()
