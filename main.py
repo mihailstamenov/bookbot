2c1588f4e3f98a3feda369fcd77e92de97b9ad94
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-08 19:19:48 +0200

    Add char count

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main ():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_word_count(text)
+    char_count = get_character_count(text)
     print(f"{word_count} words found in the document")
+    print(char_count)
     
 
 def get_book_text(path):
@@ -12,7 +14,32 @@ def get_book_text(path):
 
 def get_word_count(text):
     split = text.split()
+    # print(split)
     return len(split)
 
+def get_character_count(text):
+    dict = {}
+    for char in text:
+        char = char.lower()
+        if char in dict:
+            dict[char] = dict[char] + 1
+        else:
+            dict[char] = 1
+    return dict
 
 main()
+
+
+# alphabet = 'abcdefghijklmnopqrstuvwxyz\'.ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# numbers = '0123456789'
+# symbols = '\'`., ()[]-'
+
+# dict = {}
+# for char in alphabet:
+#         char = char.lower()
+#         if char in dict:
+#             dict[char] = dict[char]+1
+#         else:
+#             dict[char] = 1
+
+# print(dict)
